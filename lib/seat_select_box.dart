e4423822fcd1b07bbfda6f4215363c613857c337
ilsong963 <ilsong963@naver.com>
2025-03-24 18:49:48 +0900

    feat: seatSelectBox 생성

---
+++ lib/seat_select_box.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SeatSelectBox extends StatelessWidget {
+  const SeatSelectBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('SeatSelectBox');
+  }
+}
